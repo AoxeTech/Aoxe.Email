9a9b52c811b74bafbca1376d789b099de6601552
RomanDu <aeondxf@live.com>
2024-08-29 18:06:20 +0800

    Fix unimplement methord.

--- src/Aoxe.SmtpClient/AoxeSmtpClientServiceCollectionExtensions.cs
+++ src/Aoxe.SmtpClient/AoxeSmtpClientServiceCollectionExtensions.cs
@@ -2,8 +2,22 @@
 
 public static class AoxeSmtpClientServiceCollectionExtensions
 {
-    public static IServiceCollection AddSmtpClient(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddSmtpClient(
+        this IServiceCollection serviceCollection,
+        string host,
+        int port = 2525,
+        string? userName = null,
+        string? password = null,
+        bool? enableSsl = null
+    )
     {
+        serviceCollection.AddScoped<IEmailProvider>(_ => new SmtpProvider(
+            host,
+            port,
+            userName,
+            password,
+            enableSsl
+        ));
         return serviceCollection;
     }
 }
