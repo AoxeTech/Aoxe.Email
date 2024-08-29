970f602426f85f6b531a6b6ec91ce435286d581f
RomanDu <aeondxf@live.com>
2024-08-29 10:23:51 +0800

    Dev

---
+++ src/Aoxe.Email.Abstractions/EmailExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoxe.Email.Abstractions;
+
+public static class EmailExtensions
+{
+    public static ValueTask SendByAsync(
+        this Models.Email email,
+        IEmailProvider emailProvider,
+        CancellationToken cancelToken = default
+    ) => emailProvider.SendAsync(email, cancelToken);
+}
--- src/Aoxe.Email.Abstractions/Models/Defaults.cs
+++ src/Aoxe.Email.Abstractions/Models/Defaults.cs
@@ -2,6 +2,28 @@
 
 public static class Defaults
 {
-    public static readonly string ContentType =
-        $"{MediaType.Application}/{MediaSubType.OctetStream}";
+    #region MediaType
+
+    public const string MediaTypeText = "text";
+    public const string MediaTypeImage = "image";
+    public const string MediaTypeAudio = "audio";
+    public const string MediaTypeVideo = "video";
+    public const string MediaTypeApplication = "application";
+    public const string MediaTypeMultipart = "multipart";
+    public const string MediaTypeMessage = "message";
+
+    #endregion
+
+    #region MediaSubType
+
+    public const string MediaSubTypeHtml = "html";
+    public const string MediaSubTypePlain = "plain";
+    public const string MediaSubTypeXml = "xml";
+    public const string MediaSubTypeJson = "json";
+    public const string MediaSubTypeOctetStream = "octet-stream";
+    public const string MediaSubTypeUrlEncoded = "x-www-form-urlencoded";
+
+    #endregion
+
+    public const string ContentType = $"{MediaTypeApplication}/{MediaSubTypeOctetStream}";
 }
